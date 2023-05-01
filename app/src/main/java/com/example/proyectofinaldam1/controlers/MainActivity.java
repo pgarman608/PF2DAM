0e3980bdbcbd7aa68ba2d0e63993938cd013de0e
Pablo <instypablo@gmail.com>
2023-05-01 02:08:12 +0200

    1 dia Trabajo Real: 3h

--- app/src/main/java/com/example/proyectofinaldam1/controlers/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldam1/controlers/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.proyectofinaldam1.controlers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.proyectofinaldam1.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button logBtn;
     private Button tnrBtn;
     private Button rnkBtn;
@@ -19,6 +21,31 @@ public class MainActivity extends AppCompatActivity {
         logBtn = (Button) findViewById(R.id.btnLog);
         tnrBtn = (Button) findViewById(R.id.btnTnr);
         rnkBtn = (Button) findViewById(R.id.btnRnk);
+        logBtn.setOnClickListener(this);
+        tnrBtn.setOnClickListener(this);
+        rnkBtn.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intentHome = null;
+        switch (view.getId()){
+            case R.id.btnLog:
+                intentHome = new Intent(MainActivity.this, LoggingActivity.class);
+                startActivity(intentHome);
+                break;
+
+            case R.id.btnTnr:
+                intentHome = new Intent(MainActivity.this, TournamentActivity.class);
+                startActivity(intentHome);
+                break;
+
+            case R.id.btnRnk:
+                intentHome = new Intent(MainActivity.this, RankingActivity.class);
+                startActivity(intentHome);
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/proyectofinaldam1/controlers/RankingActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinaldam1.controlers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.proyectofinaldam1.R;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
